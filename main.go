2bcdc82123fb028ab86ed1dbbcf89a7be719a90c
Better <gutianyu9871@gmail.com>
2024-12-04 10:15:58 +0800

    gin-demp

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	settings "GIN_WEB/pkg"
+)
+
 func main() {
+	router := gin.Default()
+
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "test",
+		})
+	})
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", settings.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    settings.ReadTimeout,
+		WriteTimeout:   settings.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
